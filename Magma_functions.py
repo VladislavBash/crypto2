06704e12831ba9b038996772eb0ad3b756afd01e
VladislavBash <vobashara@miem.hse.ru>
2022-12-14 21:01:54 +0300

    updated

--- Magma_functions.py
+++ Magma_functions.py
@@ -18,7 +18,7 @@ def gen_key_list(key) -> list:
     return k_lst
 
 def complement_block(a) -> str:
-    if len(a) == 32:
+    if len(a) % 32 == 0:
         return a
     else:
         return a + '1' + (32-1-len(a))*'0'
@@ -43,16 +43,31 @@ def replacement_table(a) -> str:
     return res
 
 def summ_mod_2_step_32(a, k) -> int:
-    return (int(a, 2) + int(k, 2)) % pow(2, 32)
+    if type(a) == str:
+        a = int(a, 2)
+    if type(k) == str:
+        k = int(k, 2)
+    return (a + k) % pow(2, 32)
 
 def g(k, a) -> str:
     return bit_shift(replacement_table(fix_bin(summ_mod_2_step_32(a, k), 32)))
 
 def G(k, a1, a0) -> tuple:
-    return (a0, g(k, a0) ^ a1)
+    if type(a0) == str:
+        a0 = int(a0, 2)
+    if type(a1) == str:
+        a1 = int(a1, 2)
+    t = int(g(k, a0) , 2) ^ a1
+    return (fix_bin(a0, 32), fix_bin(t, 32))
 
 def G_star(k, a1, a0) -> str:
-    return (g(k, a0) ^ a1) + a0
+    # return (g(k, a0) ^ a1) + a0
+    if type(a0) == str:
+        a0 = int(a0, 2)
+    if type(a1) == str:
+        a1 = int(a1, 2)
+    t = int(g(k, a0) , 2) ^ a1
+    return fix_bin(t, 32) + fix_bin(a0, 32)
 
 def get_encrypt_keys(k_lst) -> list:
     p = [i for i in k_lst]
@@ -65,16 +80,16 @@ def get_encrypt_keys(k_lst) -> list:
     return k
 
 def encrypt(key, a) -> str:
-    a = bin(a)[2:]
+    a = fix_bin(a, 64)
     a = complement_block(a)
-    a0 = a[:32]
-    a1 =  a[32:]
+    a1 = a[:32]
+    a0 =  a[32:]
     k_lst = gen_key_list(key)
     k = get_encrypt_keys(k_lst)
     arg = (a1, a0)
     for i in range(31):
         arg = G(k[i], arg[0], arg[1])
-    return G_star(k[31], arg)
+    return G_star(k[31], arg[0], arg[1])
 
 def get_decrypt_keys(k_lst) -> list:
     k = [i for i in k_lst]
@@ -85,13 +100,13 @@ def get_decrypt_keys(k_lst) -> list:
     return k
 
 def decrypt(key, a) -> str:
-    a = bin(a)[2:]
+    a = fix_bin(a, 64)
     a = complement_block(a)
-    a0 = a[:32]
-    a1 =  a[32:]
+    a1 = a[:32]
+    a0 =  a[32:]
     k_lst = gen_key_list(key)
-    k = get_decrypt_keys(k_lst)
+    k = get_encrypt_keys(k_lst)
     arg = (a1, a0)
-    for i in range(32,0,-1):
+    for i in range(31,0,-1):
         arg = G(k[i], arg[0], arg[1])
-    return G_star(k[0], arg)
+    return G_star(k[0], arg[0], arg[1])
